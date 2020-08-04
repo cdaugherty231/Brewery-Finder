d8c96308a69e8652b168a1375f7aabb70f2bfcff
cdaugherty231 <connerodaugherty2488@gmail.com>
2020-08-04 14:57:09 -0400

    Revised DB - dropped address table

--- backend-java/database/brewerytable.sql
+++ backend-java/database/brewerytable.sql
@@ -1,5 +1,5 @@
 
-    CREATE TABLE address (
+ /*CREATE TABLE address (
      breweryaddressid SERIAL,
      streetaddress VARCHAR (99 ),
      city VARCHAR (99 ),
@@ -11,23 +11,27 @@
     );
 
 CREATE TABLE brewery (
-     breweryid SERIAL,
+     brewery_id SERIAL,
      name VARCHAR (99 ),
-     breweryaddressid INT,
+     address_street_number INT,
+     address_city VARCHAR (99 ),
+     address_state VARCHAR (99), 
+     address_zip INT,
+     phone_number VARCHAR (99 ),
      history VARCHAR (999), 
-     days_operations VARCHAR(99),
-     hours_operationid VARCHAR(99),
-     constraint pk_breweryid primary key (breweryid),
-     constraint fk_breweryaddressid foreign key (breweryaddressid) references address (breweryaddressid)
+     days_operation VARCHAR(99),
+     hours_operation VARCHAR(99),
+     constraint pk_brewery_id primary key (brewery_id),
+     /*constraint fk_breweryaddressid foreign key (breweryaddressid) references address (breweryaddressid)
      /* IMAGE Database Brewery*/
-    );
+    /*);
 
     /*DROP TABLE brewery;
 
-    DROP TABLE address; */
+      DROP TABLE address; */
 
     
-INSERT INTO address(streetaddress, city, statetwochar, zip,phonenumber ) VALUES ('27 W', 'Covington', 'KY', 41011, '859-261-5600');
+/*INSERT INTO address(streetaddress, city, statetwochar, zip,phonenumber ) VALUES ('27 W', 'Covington', 'KY', 41011, '859-261-5600');
 
 INSERT INTO brewery(name, breweryaddressid, history, days_operations,  hours_operationid ) VALUES ('Braxton', 1, 'Two Story Brewery in the heart of Covington', 'Mon-Sun', '10:00am - 12:00pm');
     
@@ -35,4 +39,4 @@ INSERT INTO brewery(name, breweryaddressid, history, days_operations,  hours_ope
     FROM brewery;
 
      SELECT *
-    FROM address;
+    FROM address; */
--- backend-java/database/schema.sql
+++ backend-java/database/schema.sql
@@ -21,38 +21,21 @@ CREATE TABLE users (
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
-
-
-    CREATE TABLE address (
-     breweryaddressid SERIAL,
-     streetaddress VARCHAR (99 ),
-     city VARCHAR (99 ),
-     statetwochar VARCHAR (99), 
-     zip int,
-     phonenumber VARCHAR (99 ),
-     constraint pk_breweryaddressid primary key (breweryaddressid)
-     
-    );
-
+ 
 CREATE TABLE brewery (
-     breweryid SERIAL,
+     brewery_id SERIAL,
      name VARCHAR (99 ),
-     breweryaddressid INT,
+     address_street INT,
+     address_city VARCHAR (99 ),
+     address_state VARCHAR (99), 
+     address_zip INT,
+     phone_number VARCHAR (99 ),
      history VARCHAR (999), 
-     days_operations VARCHAR(99),
-     hours_operationid VARCHAR(99),
-     constraint pk_breweryid primary key (breweryid),
-     constraint fk_breweryaddressid foreign key (breweryaddressid) references address (breweryaddressid)
-     /* IMAGE Database Brewery*/
-    );
+     days_operation VARCHAR(99),
+     hours_operation VARCHAR(99),
+     constraint pk_brewery_id primary key (brewery_id)
+);
 
-    /*DROP TABLE brewery;
-
-    DROP TABLE address; */
-
-    
-INSERT INTO address(streetaddress, city, statetwochar, zip,phonenumber ) VALUES ('27 W', 'Covington', 'KY', 41011, '859-261-5600');
-
-INSERT INTO brewery(name, breweryaddressid, history, days_operations,  hours_operationid ) VALUES ('Braxton', 1, 'Two Story Brewery in the heart of Covington', 'Mon-Sun', '10:00am - 12:00pm');
+INSERT INTO brewery(name, address_street , address_city, address_state,  address_zip, phone_number, history, days_operation, hours_operation) VALUES ('Braxton','27 W', 'Covington', 'KY', 41011, '859-261-5600', 'Two Story Brewery in the heart of Covington', 'Mon-Sun', '10:00am - 12:00pm');
 
 COMMIT TRANSACTION;
