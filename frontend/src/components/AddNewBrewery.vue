6dd7bd0046696e425962c2f5c9f83bb65c8ddcd0
Greyson Marks <marskgn1@gmail.com>
2020-08-04 14:44:37 -0400

    css style for frontend

--- frontend/src/components/AddNewBrewery.vue
+++ frontend/src/components/AddNewBrewery.vue
@@ -2,16 +2,30 @@
   <div>
     <form class="findUser">
       <div>
-        <input type="text" name="name" placeholder="Brewery Name" v-model="brewery.name" />
-        <input type="number" name="address" placeholder="Address" v-model="brewery.addressId" />
-        <input type="text" name="hours-opertation" placeholder="Hours of Opertion" v-model="brewery.hrsOfOperation" />
-        <input type="text" name="days-opertation" placeholder="Days of Opertion" v-model="brewery.daysOfOperation" />
-        <input type="number" name="address" placeholder="Address" v-model="brewery.addressId" />
-        <!--<input type="text" name="city" placeholder="City" v-model="brewery.city" />
-        <input type="text" name="state" placeholder="State" v-model="brewery.state" />
-        <input type="text" name="zipcode" placeholder="Zipcode" v-model="brewery.zipcode" />
-        <input type="text" name="contact-info" placeholder="Phone Number" v-model="brewery.contactInfo" />-->
-        <input type="text-area" name="history" placeholder="Description" v-model="brewery.historyBio" />
+        <li>
+          <input type="text" name="name" placeholder="Brewery Name" v-model="brewery.name" /> 
+        </li>
+        <li class="sidebyside">
+          <input  type="text" name="hours-opertation" placeholder="Hours of Opertion" v-model="brewery.hrsOfOperation" /> 
+          <input  type="text" name="days-opertation" placeholder="Days of Opertion" v-model="brewery.daysOfOperation" /> 
+        </li>
+        <li>
+          <input type="number" name="address" placeholder="Address" v-model="brewery.addressId" /> 
+        </li>
+        <li>
+          <input type="text" name="city" placeholder="City" v-model="brewery.city" />
+          </li>
+         <li> <input type="text" name="state" placeholder="State" v-model="brewery.state" />
+         </li>
+         <li>
+          <input type="text" name="zipcode" placeholder="Zipcode" v-model="brewery.zipcode" />
+        </li>
+          <li>
+          <input type="text" name="contact-info" placeholder="Phone Number" v-model="brewery.contactInfo" />
+        </li>
+        <li>
+          <input type="text-area" name="history" placeholder="Description" v-model="brewery.historyBio" />
+        </li>
       </div>
       <div>
       </div>
@@ -21,7 +35,7 @@
 </template>
 
 <script>
-import BreweryService from "@/services/BreweryService.js";
+import BreweryService from "@/services/BreweryService.js"; 
 
 export default {
   data() {
@@ -50,7 +64,19 @@ export default {
 
 <style>
 .findUser {
-  margin: 10px;
-  font-size: 1rem;
+  margin:10px auto;
+	max-width: 400px;
+	padding: 20px 12px 10px 20px;
+	font: 13px "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+}
+.findUser li {
+	padding: 0;
+	display: block;
+	list-style: none;
+	margin: 10px 0 0 0;
+}
+.sidebyside {
+  padding: 10px 0px 10px 0px
 }
 </style>
+
