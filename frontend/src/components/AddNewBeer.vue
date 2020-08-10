8008d2c59bb685feddca0eddef5495fffdee2f1a
Salvador Mendoza <salmendoza67@gmail.com>
2020-08-10 13:51:30 -0400

    add new beer css

--- frontend/src/components/AddNewBeer.vue
+++ frontend/src/components/AddNewBeer.vue
@@ -1,16 +1,16 @@
 <template>
-    <div>
-        <h1> Add New Beer to a Brewery's Beer List </h1>
+    <div class="main container">
+        <h1 class="white-text"> Add New Beer to a {{breweryName}}'s Beer List </h1>
 
-        <form class="addBeer-form-style">
-            <li>
+        <form class="form-control">
+            <li class="register-box">
                 <input 
                     type="text"
                     name="beer_name"
                     placeholder="New Beer Name"
                     v-model="newBeer.beer_name" />
             </li>
-            <li>
+            <li class="register-box">
                 <input
                     type="text-area"
                     rows="4" cols="30"
@@ -18,30 +18,28 @@
                     placeholder="Describe your new beer"
                     v-model="newBeer.beer_description" />
             </li>
-            <li>
+            <li class="register-box">
                 <input
                     type="text"
                     name="abv"
                     placeholder="Alchohol by Volume"
                     v-model="newBeer.abv"/>
             </li>
-            <li>
+            <li class="register-box">
                 <input
                     type="text"
                     name="beer_type"
                     placeholder="Beer Type"
                     v-model="newBeer.beer_type"/>
             </li>
-            <li>
-                
-            </li>
-            <li>
-                <h3> PLACEHOLDER FOR BEER IMAGE </h3>
+            <li class="register-box">
+                <h6 class="white-text"> PLACEHOLDER FOR BEER IMAGE </h6>
             </li>
             <button
+                class="btn"
                 type="submit"
                 v-on:click.prevent="submitNewBeer()"
-                >Enter</button>
+                >Submit New Beer</button>
         </form>
 
     </div>
@@ -82,5 +80,39 @@ export default {
 </script>
 
 <style>
+
+
+.white-text {
+    color: white;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+}
+
+
+.form-control {
+  border-radius: 25px;
+  font-size: 30px;
+  padding: 10px;
+  border:none;	
+  outline:none;
+}
+
+.form-control .register-box {
+  display: flex;
+  font-size: 15px;
+  justify-content: center;
+  text-align: center;
+  margin: 20px 0;
+  padding: 10px;
+}
+
+.btn {	
+ padding: 10px;
+ margin: 10px;
+ font-size: 20px;
+ border-radius: 5px;
+ background-color: rgb(247, 206, 71);
+}
 
 </style>
