77589592c09cbd29f73b1a2ba67c1525095b223c
cdaugherty231 <connerodaugherty2488@gmail.com>
2020-08-12 11:28:36 -0400

    Adding fixed Home background

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -12,14 +12,15 @@
       </div>
       </b-container>
     
-
+    <b-container class="bv-brewer-home" fluid= "xl">
     <div
       class="Brewer"
-      v-if="$store.state.user.authorities.find(auth => auth.name == 'ROLE_BREWER')"
-    >
+      v-if="$store.state.user.authorities.find(auth => auth.name == 'ROLE_BREWER')">
       <!--<brewery-update></brewery-update> Tien's Change-->
       <brewery-list></brewery-list>
     </div>
+    </b-container>
+
     <div
       class="Beer-Lover"
       v-if="$store.state.user.authorities.find(auth => auth.name == 'ROLE_USER')"
@@ -48,20 +49,17 @@ export default {
  },
 };
 </script>
-<style scope>
-.Admin-add-brewery-card {
-  background-color: lightblue;
-}
+<style>
 
 .home {
-  background-color: lightblue;
   font-family: "Noto Sans", sans-serif;
   background: linear-gradient(rgba(255, 216, 155, 0.4), rgba(25, 84, 123, 0.8)),
     url(https://www.wvxu.org/sites/wvxu/files/201409/Beer.JPG) no-repeat center
       center fixed;
   background-size: 100% 100%;
-  height: 100%;
-  width: 100%;
+  position: absolute;
+  height: 100vh; /* Makes background full size per page- do not touch!!!!*/
+  width: 100%; 
 }
 </style>
 
